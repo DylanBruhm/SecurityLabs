f7b9008187c1d784e53bae008815441b6033837d
DylanBruhm <dylanrileybruhm@gmail.com>
2026-04-17 20:36:53 -0300

    Login Page
    Built a Flask login page that takes user input from an HTML form and checks if the credentials match. This is part of a security lab project where I’m building out a realistic web app to later test vulnerabilities like SQL injection and brute force attacks.

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "My Security Lab is running"
+    return redirect(url_for("login")) 
+
+@app.route("/data")
+def data():
+    user_input = request.args.get("input")
+
+    if user_input:
+        return f"You entered: {user_input}"
+    else:
+        return "No input provided"
+    
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("pirate")
+        password = request.form.get("shores")
+
+        if username != "pirate" or password != "shores":
+            return render_template("login.html", error="Invalid Pirate")
+        else:
+            return f"Welcome: {username}"
+
+    return render_template("login.html")
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
+
